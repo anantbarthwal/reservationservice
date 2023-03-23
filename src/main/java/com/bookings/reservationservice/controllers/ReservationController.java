78e3dc55fa407a6c72ecab8426942766ac2a215d
Anant Barthwal <anant.barthwal@citiustech.com>
2023-03-24 02:37:06 +0530

    added accountRateBreaker (circuitbreaker)

--- src/main/java/com/bookings/reservationservice/controllers/ReservationController.java
+++ src/main/java/com/bookings/reservationservice/controllers/ReservationController.java
@@ -3,6 +3,7 @@ package com.bookings.reservationservice.controllers;
 import com.bookings.reservationservice.handlers.ReservationHandler;
 import com.bookings.reservationservice.models.ReservationCreateRequest;
 import com.bookings.reservationservice.models.ReservationSummary;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +14,15 @@ public class ReservationController {
     @Autowired
     private ReservationHandler reservationHandler;
     @PostMapping("/create")
+    @CircuitBreaker(name= "accountRateBreaker", fallbackMethod = "accountRateFallBack")
     public ReservationSummary createReservation(@RequestBody ReservationCreateRequest reservationCreateRequest) {
         return reservationHandler.createReservation(reservationCreateRequest);
+    }
+
+    // creating accountRateFallBack method for circuitBreaker
+    public ReservationSummary accountRateFallBack(ReservationCreateRequest reservationCreateRequest, Exception ex) {
+        ReservationSummary reservationSummary = new ReservationSummary();
+        reservationSummary.setUserId("this is fallback method");
+        return reservationSummary;
     }
 }
